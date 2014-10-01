6601ec04f478a890f1de44c0d449590175501ee9
tonimichel-hdm <tm063@hdm-stuttgart.de>
2014-10-01 09:56:40 +0200

    working prototype

--- src/android/SensorBridge.java
+++ src/android/SensorBridge.java
@@ -11,31 +11,24 @@ import android.content.Intent;
 
 public class SensorBridge extends CordovaPlugin {
 	
-	public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
+	public static final String ACTION_CALL_ECHO = "echo";
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		try {
-			if(ACTION_ADD_CALENDAR_ENTRY.equals(action)) {
-				JSONObject arg_object = args.getJSONObject(0);
-				Intent callIntent = new Intent(Intent.ACTION_EDIT)
-					.setType("vnd.android.cursor.item/event")
-				 	.putExtra("beginTime", arg_object.getLong("startTimeMillis"))
-			        .putExtra("endTime", arg_object.getLong("endTimeMillis"))
-			        .putExtra("title", arg_object.getString("title"))
-			        .putExtra("description", arg_object.getString("description"))
-			        .putExtra("eventLocation", arg_object.getString("eventLocation"));
-				 
-				 	this.cordova.getActivity().startActivity(callIntent);
-				 	callbackContext.success();
-				 	return true;
-			}
-			callbackContext.error("Invalid action");
-			return false;
-		} catch (Exception e) {
-			System.err.println("Exception: " + e.getMessage());
-			callbackContext.error(e.getMessage());
-			return false;
+		if(ACTION_CALL_ECHO.equals(action)) {
+			String message = args.getString(0);
+			String attachment = ": ... it really went full circle!";
+			message += attachment;
+			this.echo( message );
+			return true;
+		}
+	}
+	
+	private void echo (String message CallbackContext callbackContext) {
+		if( message != null && message.length() > 0 ) {
+			callbackContext.success( message );
+		} else {
+			callbackContext.error("Expected non-empty string argument.");
 		}
 	}
 	
