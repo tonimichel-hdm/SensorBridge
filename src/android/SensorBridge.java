ad3d3d951db36408fc0cb393e00380f6cd95b685
tonimichel-hdm <tm063@hdm-stuttgart.de>
2014-10-01 09:41:56 +0200

    newcommit

--- src/android/SensorBridge.java
+++ src/android/SensorBridge.java
@@ -1,1 +1,42 @@
-package org.tonimichelhdm.SensorBridge; import org.apache.cordova.CallbackContext;import org.apache.cordova.CordovaPlugin;import org.apache.cordova.api.PluginResult;import org.json.JSONObject;import org.json.JSONArray;import org.json.JSONException;import android.content.Intent;import android.hardware.Sensor;import android.hardware.SensorManager;public class SensorBridge extends CordovaPlugin {		public static final String ACTION_GET_SENSORLIST = "getSensorList";	public static final String ACTION_GET_ECHO = "getEcho";		@Override	public boolean execute ( String action, JSONArray args, CallbackContext callbackContext) throws JSONEXception {	/*	if(ACTION_GET_ECHO.equals(action)){			String message = args.getString(0);			this.echo(message);			return true;		}		return false;	*/	}		private void echo(String message, CallbackContext callbackContext) {	/*	if( message != null && message.length() > 0) {			callbackContext.success(message + ".. Wow that worked");		}		callbackContext.error("Expected non-empty string argument");	*/	}		}
+package org.tonimichelhdm.sensorbridge;
+ 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class SensorBridge extends CordovaPlugin {
+	
+	public static final String ACTION_ADD_CALENDAR_ENTRY = "addCalendarEntry";
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		try {
+			if(ACTION_ADD_CALENDAR_ENTRY.equals(action)) {
+				JSONObject arg_object = args.getJSONObject(0);
+				Intent callIntent = new Intent(Intent.ACTION_EDIT)
+					.setType("vnd.android.cursor.item/event")
+				 	.putExtra("beginTime", arg_object.getLong("startTimeMillis"))
+			        .putExtra("endTime", arg_object.getLong("endTimeMillis"))
+			        .putExtra("title", arg_object.getString("title"))
+			        .putExtra("description", arg_object.getString("description"))
+			        .putExtra("eventLocation", arg_object.getString("eventLocation"));
+				 
+				 	this.cordova.getActivity().startActivity(callIntent);
+				 	callbackContext.success();
+				 	return true;
+			}
+			callbackContext.error("Invalid action");
+			return false;
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			callbackContext.error(e.getMessage());
+			return false;
+		}
+	}
+	
+}
